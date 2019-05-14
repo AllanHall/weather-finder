53c4c2bd0c592ce6794f5804eae17e4efb02ceff
Allan Hall <bigal76126@gmail.com>
2019-05-14 13:29:06 -0400

    made search function to take the value out of text box

--- public/main.js
+++ public/main.js
@@ -1,16 +1,29 @@
+let city = {}
+let zipcode = {}
+
 fetch('api.openweathermap.org/data/2.5/weather?q={city name}')
   .then(resp => {
     if (resp.status === 200) {
       return resp.json()
     } else {
-      displayErrorMessage(resp)
+      console.log('error')
     }
   })
-  .then(json => {
-    displayData(json)
+  .then(city => {
+    displayData(city)
+  }
+  fetch('api.openweathermap.org/data/2.5/weather?zip={zip code}')
+    .then(zipcode => {
+    displayData(zipcode)
   })
+)
 
 const main = () => {}
 
+const searchFunction = (name) => {
+  city = document.querySelector('.city-zip').value
+  //run this value through the api
+}
+
 document.addEventListener('DOMContentLoaded', main)
-document.querySelector('.search').addEventListener('click', searchFuction)
+document.querySelector('.search').addEventListener('click', searchFunction)
