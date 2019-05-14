8713c1ab255cb2c60cc1b348be0ab5f45829d9db
Allan Hall <bigal76126@gmail.com>
2019-05-14 13:08:11 -0400

    added general fetch function to js

--- public/main.js
+++ public/main.js
@@ -1,3 +1,15 @@
+fetch('api.openweathermap.org/data/2.5/weather?q={city name}')
+  .then(resp => {
+    if (resp.status === 200) {
+      return resp.json()
+    } else {
+      displayErrorMessage(resp)
+    }
+  })
+  .then(json => {
+    displayData(json)
+  })
+
 const main = () => {}
 
 document.addEventListener('DOMContentLoaded', main)
